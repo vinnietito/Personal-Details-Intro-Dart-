5bfa79486465a28ccc020c2492d9b6507de25ba9
vinnietito <vincentnjuguna69@gmail.com>
2024-10-28 11:29:17 +0300

    Initial Commit

---
+++ personalDetails.dart
@@ -0,0 +1,16 @@
+void main() {
+  var name = "Vincent Kimanthi";
+  var hobby = "Playing Play station";
+  var age = 20;
+  var country = "Kenya";
+  var willdrive = "GLC 43AMG";
+
+  print(name);
+  print(hobby);
+  print(age);
+  print(country);
+  print(willdrive);
+
+  //Lets make a logical sentense of the student details
+  print("My name is $name and I am $age years old. I live in $country and my hobby is $hobby. I will drive a $willdrive" );
+}
